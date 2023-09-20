af5f1c374816db9fbc8057f453504f5cdb1bfba2
Ismail Korir <ismail.korir1997@gmail.com>
2023-09-20 01:10:04 +0100

    functions scope

---
+++ javascript-sandbox-start/03-functions-scope/02-params-arguments/script.js
@@ -0,0 +1,8 @@
+function getRandom(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const item = arr[randomIndex];
+
+    console.log(item);
+}
+
+getRandom([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
---
+++ javascript-sandbox-start/03-functions-scope/03-global-function-scope/script.js
@@ -0,0 +1,12 @@
+function run() {
+    console.log(window.innerHeight);
+}
+
+run();
+
+function add() {
+    const y = 50;
+    console.log(y)
+}
+
+add()
