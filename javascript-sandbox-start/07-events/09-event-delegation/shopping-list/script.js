0c2636eed25d6ce681daed6d619ac0b6684bd307
Ismail Korir <ismail.korir1997@gmail.com>
2023-10-06 15:17:53 +0100

    Shopping list

---
+++ javascript-sandbox-start/07-events/09-event-delegation/shopping-list/script.js
@@ -0,0 +1,12 @@
+const listItems = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+// listItems.forEach((item) => {
+//     item.addEventListener('click', (e) => {
+//     e.target.remove();
+//     })
+// })
+
+list.addEventListener('click', (e) => {
+    console.log(e.target);
+});
