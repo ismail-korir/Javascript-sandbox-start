7e7b984cdc68f1111002bd8630629c76a7f77eef
Ismail Korir <ismail.korir1997@gmail.com>
2023-10-09 11:10:08 +0100

    Fetch API

---
+++ javascript-sandbox-start/10-fetch-and-async-await/01-fetch-basics/script.js
@@ -0,0 +1,15 @@
+// Fetching json file
+fetch('./movies.json')
+.then((response) => response.json())
+.then((data) => console.log(data));
+
+// Fetching a text file
+fetch('./test.txt')
+.then((response) => response.text())
+.then((data) => console.log(data));
+
+// Fetching from an API
+fetch('https://api.github.com/users')
+.then((response) => response.json())
+.then((data) => console.log(data));
+
