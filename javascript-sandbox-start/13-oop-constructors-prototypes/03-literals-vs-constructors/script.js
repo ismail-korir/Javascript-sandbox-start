0923c5cdcd6e5da66220b11bca2689ddcef3f8f4
Ismail Korir <ismail.korir1997@gmail.com>
2023-10-12 14:26:06 +0100

    OOP

---
+++ javascript-sandbox-start/13-oop-constructors-prototypes/03-literals-vs-constructors/script.js
@@ -0,0 +1,5 @@
+const strLit = 'Hello';
+const strObj = new String('Hello');
+
+console.log(strLit, typeof strLit);
+console.log(strObj, typeof strObj);
--- javascript-sandbox-start/13-oop-constructors-prototypes/04-object-properties/script.js
+++ javascript-sandbox-start/13-oop-constructors-prototypes/04-object-properties/script.js
@@ -6,3 +6,21 @@ function Rectangle(name, width, height) {
     return this.width * this.height;
   };
 }
+
+const rect1 = new Rectangle('Rectangle 1', 20, 20);
+const rect2 = new Rectangle('Rectangle 2', 20, 30);
+
+
+console.log(rect1.name, rect2.width);
+console.log(rect1['width']);
+
+rect1.color = 'red';
+
+rect2.perimeter = () => 2 * (rect2.width + rect2.height);
+
+console.log(rect2.perimeter());
+
+delete rect2.perimeter;
+
+
+console.log(rect2);
